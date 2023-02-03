c1c33bac2761362cfb95b0bae8c9292cac7f2d15
pjkw <peterjosephkwiatkowski@gmail.com>
2023-02-03 17:33:04 -0500

    map working

---
+++ RealmsOfAlerion/Assets/Scripts/Map.cs
@@ -0,0 +1,45 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Map : MonoBehaviour
+{
+    public int width = 10;
+    public int height = 10;
+
+    public float hexSize;
+
+    float verticalDistance;
+    float horizontalDistance;
+
+    public GameObject tilePrefab;
+
+    void Start()
+    {
+        // In the pointy top orientation, the horizontal distance between adjacent hexagon centers is horiz = width = sqrt(3) * size.
+        // The vertical distance is vert == 3/4 * height == 3/2 * size.
+
+        horizontalDistance = Mathf.Sqrt(3) * hexSize;
+        verticalDistance = hexSize * 1.35f; // 3 / 2 * hexSize; // okay this works, but we need a custom multiplier for now here
+
+        GenerateMap();
+    }
+
+    void GenerateMap()
+    {
+        for (int x = 0; x < width; x++)
+        {
+            for (int y = 0; y < height; y++)
+            {
+                if (y % 2 == 0)
+                {
+                    GameObject tile = Instantiate(tilePrefab, new Vector3(x * horizontalDistance, y * verticalDistance, 0), Quaternion.identity, this.transform);
+                }
+                else
+                {
+                    GameObject tile = Instantiate(tilePrefab, new Vector3(x * horizontalDistance + horizontalDistance / 2, y * verticalDistance, 0), Quaternion.identity, this.transform);
+                }
+            }
+        }
+    }
+}
