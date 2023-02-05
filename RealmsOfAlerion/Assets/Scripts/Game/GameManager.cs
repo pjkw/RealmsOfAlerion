86c8875a0d9180ca6e1f13aeef706c45830170dd
pjkw <peterjosephkwiatkowski@gmail.com>
2023-02-05 11:02:12 -0500

    game manager

---
+++ RealmsOfAlerion/Assets/Scripts/Game/GameManager.cs
@@ -0,0 +1,51 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{ 
+    // player turn enum
+    public enum PlayerTurn
+    {
+        Player1,
+        Player2,
+        Player3,
+        Player4,
+        Player5,
+    }
+
+    public PlayerTurn playerTurn;
+
+    // game phase enum
+    public enum GamePhase
+    {
+        None,
+        Buy,
+        Build,
+        GameOver
+    }
+
+    public GamePhase gamePhase;
+
+    // singleton
+    public static GameManager instance;
+
+    // game objects
+    public GameObject player;
+
+    // game data
+    public int playerTurns;
+
+    void Awake()
+    {
+        // singleton
+        if (instance == null)
+        {
+            instance = this;
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
+    }
+}
