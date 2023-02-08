a07e9c36a1bae45966951f26bee0b2541b3ee36a
pjkw <peterjosephkwiatkowski@gmail.com>
2023-02-08 16:06:51 -0500

    inventory done

--- RealmsOfAlerion/Assets/Scripts/Player/Inventory.cs
+++ RealmsOfAlerion/Assets/Scripts/Player/Inventory.cs
@@ -4,6 +4,8 @@ using UnityEngine;
 
 public class Inventory : MonoBehaviour
 {
+    public static Inventory instance;
+
     [System.Serializable]
     public class Resources
     {
@@ -14,7 +16,56 @@ public class Inventory : MonoBehaviour
         public int gold;
     }
 
+    [Header("Resources")]
     public Resources player1Resources;
     public Resources player2Resources;
 
+    // add label in inspector -- Cards in Hand
+
+    [System.Serializable]
+    public class UnactivatedCards
+    {
+        public List<GameObject> cards;
+    }
+
+    [Header("Cards In Hand")]
+    public UnactivatedCards player1UnactivatedCards;
+    public UnactivatedCards player2UnactivatedCards;
+
+    
+    [System.Serializable]
+    public class ActivatedCards
+    {
+        public List<GameObject> cards;
+    }
+
+    [Header("Purchased Cards")]
+    public ActivatedCards player1ActivatedCards;
+    public ActivatedCards player2ActivatedCards;
+
+    
+    void Awake()
+    {
+        instance = this;
+    }
+
+    void Start()
+    {
+        SetStartingResources();
+    }
+
+    void SetStartingResources()
+    {
+        player1Resources.food = 1;
+        player1Resources.wood = 1;
+        player1Resources.magic = 1;
+        player1Resources.mana = 1;
+        player1Resources.gold = 1;
+
+        player2Resources.food = 1;
+        player2Resources.wood = 1;
+        player2Resources.magic = 1;
+        player2Resources.mana = 1;
+        player2Resources.gold = 1;
+    }
 }
