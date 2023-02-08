65ac8bb1f637db317269c26b8ad30063bb4015fc
pjkw <peterjosephkwiatkowski@gmail.com>
2023-02-08 16:24:06 -0500

    session end

---
+++ RealmsOfAlerion/Assets/Scripts/Player/InventoryBar.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class InventoryBar : MonoBehaviour
+{
+    // add stone later
+    [SerializeField] Text foodText;
+    [SerializeField] Text woodText;
+    [SerializeField] Text magicText;
+    [SerializeField] Text manaText;
+    [SerializeField] Text goldText;
+
+    void Start()
+    {
+        SetInventory();
+    }
+
+    void SetInventory()
+    {
+        foodText.text = Inventory.instance.player1Resources.food.ToString();
+        woodText.text = Inventory.instance.player1Resources.wood.ToString();
+        magicText.text = Inventory.instance.player1Resources.magic.ToString();
+        manaText.text = Inventory.instance.player1Resources.mana.ToString();
+        goldText.text = Inventory.instance.player1Resources.gold.ToString();
+    }
+}
